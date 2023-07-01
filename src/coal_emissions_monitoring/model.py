5c89f74d480a82e1caf25305df40633306432cb8
Bogdan Mihailovic <bogdanmh96@gmail.com>
2023-07-01 18:07:53 +0200

    refactor: removed unsqueezing the targets to squeeze the predictions

--- src/coal_emissions_monitoring/model.py
+++ src/coal_emissions_monitoring/model.py
@@ -149,10 +149,8 @@ class CoalEmissionsModel(LightningModule):
         x, y = batch["image"], batch["target"]
         x, y = x.float().to(self.device), y.float().to(self.device)
         # forward pass (calculate predictions)
-        y = y.unsqueeze(1)
-        self.log(y)
-        self.log(y.shape)
         y_pred = self(x)
+        y_pred = y_pred.unsqueeze(1) if self.model_name == "UNet" else y_pred
         # calculate metrics for the current batch
         metrics = self.calculate_all_metrics(preds=y_pred, targets=y)
         metrics = {
